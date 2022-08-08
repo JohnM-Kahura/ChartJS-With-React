9788a05d41d829f9cad8711dc143d8c9ae59cdc0
JohnM-Kahura <Johnmungai392@gmail.com>
2022-08-08 15:17:02 +0300

    chart build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import BarChart from "./components/BarChart";
+import { useEffect,useState } from "react";
+import axios from 'axios';
+import NavBar from "./components/NavBar";
+import PieChart from "./components/PieChart";
 function App() {
+  const [data, setData] = useState([1,3,4])
+
+ 
+  const fetchData=async()=>{
+    try{
+      const res = await axios.get('http://127.0.0.1:8000/api/',{
+        headers: {
+           'accept': 'application/json',
+        }
+    })
+    console.log(res.data)
+    
+    setData(res.data)
+   
+  } catch (error) {
+      console.log(error)
+  }
+  }
+
+  useEffect(()=>{
+fetchData()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      {data.map((item)=>{
+        return( 
+        <>
+        <div className="grid grid-cols-2 ">
+
+        <div style={{width:600}}>
+
+        <BarChart chartData={{
+        labels:['Maths','English','Biology','Chemistry','History','Physics'],
+        datasets:[{
+         label:item.name,
+         data:[item.maths,item.english,item.biology,item.chemistry,item.history,item.physics],
+         backgroundColor:['red','blue','green','yellow','brown','pink']
+
+         
+        }] 
+      }}/>
+         </div>
+<div style={{width:400,height:500}}>
+<PieChart chartData={{
+  labels:['Maths','English','Biology','Chemistry','History','Physics'],
+  datasets:[{
+    label:item.name,
+    data:[item.maths,item.english,item.biology,item.chemistry,item.history,item.physics],
+    backgroundColor:['red','blue','green','yellow','brown','pink']
+    
+    
+  }] 
+}}/>
+</div>
+</div>
+         </>
+         )
+      })}
+      
+      
     </div>
   );
 }
